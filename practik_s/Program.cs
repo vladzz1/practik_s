4ad35f11b5ded178779b4c7171755277b242625e
Admin <Admin@DESKTOP-B6M267U>
2025-06-10 16:49:20 +0300

    Add project files.

---
+++ practik_s/Program.cs
@@ -0,0 +1,142 @@
+﻿using System.Text;
+using System.Xml.Linq;
+
+namespace practik_s
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello");
+            //cw + tab
+            Console.WriteLine("Hello\n");
+            Console.WriteLine("\tHello\n");
+            Console.WriteLine("\t\tHello\n");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+
+            object obj = new object();
+            //Int32 a = 16;
+            int a = 16;
+            int b = 4;
+            Console.WriteLine(b);
+
+            short d = 41;
+            Single g = 1.33f;//double ---> float
+            float h = 8.23F;
+
+            double pi = 3.14;
+            double g2 = 8.8;
+
+            decimal p = 5.54m;
+
+            Console.WriteLine("enter number: ");
+            string str = Console.ReadLine()!;//100
+            Console.WriteLine(str + "!!!!");
+            Console.WriteLine(str + 10 + "!!!!");
+            int number = int.Parse(str);
+            Console.WriteLine(number + 10 + "!!!!");
+            Console.WriteLine("you entered: " + number + "!!!!");
+            Console.WriteLine("you entered: " + number + 100 + " !!!!");
+            Console.WriteLine("you entered: " + (number + 100) + " !!!!");
+            Console.WriteLine($"you entered:  + {number + 100} + !!!!");
+            //not null - value types
+            int v;
+            int u = 0;
+            //Nullable<int> y = null;
+            int? y = null;
+            //NULLable
+            string word = null;
+            string word2 = "hello world";
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("привіт світ");
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            Random random = new Random();
+            for (short i = 0; i < 10; i++)
+            {
+                Console.WriteLine(random.Next(10, 100));
+                Console.WriteLine(random.Next(100));
+                Console.WriteLine(random.Next(100, 500));
+                Console.WriteLine(random.NextDouble());
+            }
+
+            Console.WriteLine(random.Next());//0....maxInt
+            Console.WriteLine(random.Next(100));//0....99
+            Console.WriteLine(random.Next(100, 500));//100....499
+            Console.WriteLine(random.NextDouble());//0....1
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.ResetColor();
+
+            Console.WriteLine((10).GetType());
+            Console.WriteLine((10D).GetType());
+            Console.WriteLine((10f).GetType());
+            Console.WriteLine((10m).GetType());
+            Console.WriteLine((10L).GetType());
+            Console.WriteLine((10UL).GetType());
+            Console.WriteLine(0xFF);
+
+            Console.WriteLine($"ToShortTimeString : {now.ToShortTimeString()}");
+            Console.WriteLine($"ToShortDateString : {now.ToShortDateString()}");
+            Console.WriteLine($"ToLongTimeString : {now.ToLongTimeString()}");
+            Console.WriteLine($"ToLongDateString : {now.ToLongDateString()}");
+            Console.WriteLine($"Custom : {now.ToString("yyyy/MM/dd")}");
+
+            //int a = 5;
+            ////implicit
+            //float d = a;//5.0000000
+            ////explicit
+            //int b = (int)3.14;
+            ////Nullable<int> num2 = null;
+            ////or
+            //int? num2 = null;
+
+            //string str = null;
+            //string str3 = "Hello";
+            //string str2 = str;
+
+            //if (str != null)
+            //    str.ToUpper();
+            ////or   ---> ? - not null //? - null-conditional operator
+            //str?.ToLower();
+
+            //str2 = (str == null ? "Error" : str);
+            ////or
+            //if (str == null)
+            //{
+            //    str2 = "Error";
+            //}
+            //else
+            //{
+            //    str2 = str;
+            //}
+            ////or  //?? - null-conditional operator
+            //str2 = str ?? "Error";
+
+            //explicit
+            //int i2 = (int)f2;
+
+            //try
+            //{
+            //    numberStr = Console.ReadLine();
+            //    float float_number = Convert.ToSingle(numberStr);
+            //    Console.WriteLine($"Number float {numberStr}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.WriteLine("Continue...............");
+            //Console.WriteLine("Continue...............");
+            //Console.WriteLine("Continue...............");
+            //Console.WriteLine("Continue...............");
+        }
+    }
+}
---
+++ practik_s_2/Program.cs
@@ -0,0 +1,162 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace practik_s_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = new object();
+
+            //завдання 1
+
+            Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup\n");
+            Console.WriteLine("---------------------------\n");
+
+            //завдання 2
+
+            Console.WriteLine("введіть перше число");
+            string str1 = Console.ReadLine()!;
+            Console.WriteLine("введіть друге число");
+            string str2 = Console.ReadLine()!;
+            Console.WriteLine("введіть третє число");
+            string str3 = Console.ReadLine()!;
+            Console.WriteLine("введіть четверте число");
+            string str4 = Console.ReadLine()!;
+            Console.WriteLine("введіть п'яте число");
+            string str5 = Console.ReadLine()!;
+            int a1 = int.Parse(str1);
+            int a2 = int.Parse(str2);
+            int a3 = int.Parse(str3);
+            int a4 = int.Parse(str4);
+            int a5 = int.Parse(str5);
+            Console.WriteLine($"{a1} + {a2} + {a3} + {a4} + {a5} = {a1 + a2 + a3 + a4 + a5}");
+            if (a1 > a2 && a1 > a3 && a1 > a4 && a1 > a5)
+            {
+                Console.WriteLine($"максимальне: {a1}");
+            }
+            else if (a2 > a1 && a2 > a3 && a2 > a4 && a2 > a5)
+            {
+                Console.WriteLine($"максимальне: {a2}");
+            }
+            else if (a3 > a1 && a3 > a2 && a3 > a4 && a3 > a5)
+            {
+                Console.WriteLine($"максимальне: {a3}");
+            }
+            else if (a4 > a1 && a4 > a2 && a4 > a3 && a4 > a5)
+            {
+                Console.WriteLine($"максимальне: {a4}");
+            }
+            else if (a5 > a1 && a5 > a2 && a5 > a3 && a5 > a4)
+            {
+                Console.WriteLine($"максимальне: {a5}");
+            }
+            if (a1 < a2 && a1 < a3 && a1 < a4 && a1 < a5)
+            {
+                Console.WriteLine($"мінімальне: {a1}");
+            }
+            else if (a2 < a1 && a2 < a3 && a2 < a4 && a2 < a5)
+            {
+                Console.WriteLine($"мінімальне: {a2}");
+            }
+            else if (a3 < a1 && a3 < a2 && a3 < a4 && a3 < a5)
+            {
+                Console.WriteLine($"мінімальне: {a3}");
+            }
+            else if (a4 < a1 && a4 < a2 && a4 < a3 && a4 < a5)
+            {
+                Console.WriteLine($"мінімальне: {a4}");
+            }
+            else if (a5 < a1 && a5 < a2 && a5 < a3 && a5 < a4)
+            {
+                Console.WriteLine($"мінімальне: {a5}");
+            }
+            Console.WriteLine($"{a1} * {a2} * {a3} * {a4} * {a5} = {a1 * a2 * a3 * a4 * a5}\n");
+            Console.WriteLine("---------------------------\n");
+
+            //завдання 3
+
+            Console.WriteLine("введіть перше число");
+            str1 = Console.ReadLine()!;
+            Console.WriteLine("введіть друге число");
+            str2 = Console.ReadLine()!;
+            Console.WriteLine("введіть третє число");
+            str3 = Console.ReadLine()!;
+            Console.WriteLine("введіть четверте число");
+            str4 = Console.ReadLine()!;
+            Console.WriteLine("введіть п'яте число");
+            str5 = Console.ReadLine()!;
+            int a = int.Parse(str1 + str2 + str3 + str4 + str5);
+            Console.WriteLine(a);
+            a1 = a % 10;
+            a2 = a / 10 % 10;
+            a3 = a / 100 % 10;
+            a4 = a / 1000 % 10;
+            a5 = a / 10000 % 10;
+            Console.WriteLine($"{a1}{a2}{a3}{a4}{a5}\n");
+            Console.WriteLine("---------------------------\n");
+
+            //завдання 4
+
+            Console.WriteLine("введіть початок діапазону");
+            str1 = Console.ReadLine()!;
+            Console.WriteLine("введіть кінець діапазону");
+            str2 = Console.ReadLine()!;
+            int start = int.Parse(str1);
+            int end = int.Parse(str2);
+            for (int i = start; i + start < end; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---------------------------\n");
+
+            //завдання 5
+
+            Console.WriteLine("введіть перше число");
+            str1 = Console.ReadLine()!;
+            Console.WriteLine("введіть друге число");
+            str2 = Console.ReadLine()!;
+            a1 = int.Parse(str1);
+            a2 = int.Parse(str2);
+            for (int i = a1; i <= a2; i++)
+            {
+                end = i;
+                for (int j = 0; j < end; j++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("---------------------------\n");
+
+            //завдання 6
+
+            int key;
+            Console.WriteLine("введіть довжину лінії");
+            str1 = Console.ReadLine()!;
+            Console.WriteLine("введіть символ");
+            str2 = Console.ReadLine()!;
+            Console.WriteLine("введіть 1. горизонтальна лінія 2. вертикальна лінія");
+            str3 = Console.ReadLine()!;
+            a1 = int.Parse(str1);
+            key = int.Parse(str3);
+            if (key == 1)
+            {
+                for (int i = 0; i < a1; i++)
+                {
+                    Console.WriteLine(str2);
+                }
+            }
+            else if (key == 2)
+            {
+                for (int i = 0; i < a1; i++)
+                {
+                    Console.WriteLine($"{str2}\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"в меню немає {key}");
+            }
+        }
+    }
+}
