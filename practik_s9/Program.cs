aa763d9b9e814364813bfda3ed87d62fbcf75e69
Admin <Admin@DESKTOP-B6M267U>
2025-06-28 15:52:30 +0300

    Interfaces

---
+++ practik_s9/Program.cs
@@ -0,0 +1,164 @@
+﻿namespace practik_s9
+{
+    interface IOutput
+    {
+        void show();
+        void show(string info);
+    }
+    interface IMath
+    {
+        int max();
+        int min();
+        float avg();
+        bool search(int valueToSearch);
+    }
+    interface ISort
+    {
+        void sortAsc();
+        void sortDesc();
+        void sortByParam(bool isAsc);
+    }
+    class Array : IOutput, IMath, ISort
+    {
+        public int[]? Arr { get; set; }
+        public Array()
+        {
+            Arr = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Arr[i] = random.Next(0, 10);
+            }
+        }
+        public Array(int size)
+        {
+            Arr = new int[size];
+            Random random = new Random();
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                Arr[i] = random.Next(0, 10);
+            }
+        }
+        public float avg()
+        {
+            int count = 0;
+            for (int i = 0; i < Arr!.Length; i++)
+            {
+                count += Arr[i];
+            }
+            return count / Arr.Length;
+        }
+        public int max()
+        {
+            int count = Arr![0];
+            for (int i = 0; i < Arr!.Length; i++)
+            {
+                if (Arr[i] > count)
+                {
+                    count = Arr[i];
+                }
+            }
+            return count;
+        }
+        public int min()
+        {
+            int count = Arr![0];
+            for (int i = 0; i < Arr!.Length; i++)
+            {
+                if (Arr[i] < count)
+                {
+                    count = Arr[i];
+                }
+            }
+            return count;
+        }
+        public bool search(int valueToSearch)
+        {
+            for (int i = 0; i < Arr!.Length; i++)
+            {
+                if (Arr[i] == valueToSearch)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void show()
+        {
+            for (int i = 0; i < Arr?.Length; i++)
+            {
+                Console.Write($"{Arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+        public void show(string info)
+        {
+            Console.WriteLine(info);
+            for (int i = 0; i < Arr?.Length; i++)
+            {
+                Console.Write($"{Arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+        public void sortAsc()
+        {
+            for (int i = 0; i < Arr!.Length - 1; i++)
+            {
+                for (int j = 0; j < Arr!.Length - i - 1; j++)
+                {
+                    if (Arr[j] > Arr[j + 1])
+                    {
+                        int temp = Arr[j];
+                        Arr[j] = Arr[j + 1];
+                        Arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public void sortDesc()
+        {
+            for (int i = 0; i < Arr!.Length - 1; i++)
+            {
+                for (int j = 0; j < Arr!.Length - i - 1; j++)
+                {
+                    if (Arr[j] < Arr[j + 1])
+                    {
+                        int temp = Arr[j];
+                        Arr[j] = Arr[j + 1];
+                        Arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public void sortByParam(bool isAsc)
+        {
+            if(isAsc == true)
+            {
+                sortAsc();
+            }
+            else
+            {
+                sortDesc();
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Array arr = new Array();
+            arr.show();
+            arr.show("ще один масив");
+            Console.WriteLine(arr.max());
+            Console.WriteLine(arr.min());
+            Console.WriteLine(arr.avg());
+            Console.WriteLine(arr.search(4));
+            arr.sortAsc();
+            arr.show();
+            arr.sortDesc();
+            arr.show();
+            arr.sortByParam(true);
+            arr.show();
+        }
+    }
+}
---
+++ practik_s9_2/Program.cs
@@ -0,0 +1,178 @@
+﻿namespace practik_s9_2
+{
+    class MyClass
+    {
+        public void Print() { }
+    }
+    //interface IWorkable
+    //{
+    //   // private string name; // error
+    //    //public
+    //    string Work();
+    //    bool IsWorking { get; set; }
+    //    event EventHandler workEnded;
+    //}
+    abstract class Human
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public void Print()
+        {
+            Console.WriteLine($"{LastName}. Name : {FirstName}.\n" +
+                $"Birthdate : {Birthdate.ToLongDateString()}");
+        }
+        public override string ToString()
+        {
+            return $"{LastName}. Name : {FirstName}.\n" +
+                $"Birthdate : {Birthdate.ToLongDateString()}";
+        }
+    }
+    abstract class Employee : Human
+    {
+        public double Salary { get; set; }
+        public string Position { get; set; }
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"\nPosition : {Position}" +
+                $". Salary : {Salary}");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $"\nPosition : {Position}" +
+                $". Salary : {Salary}";
+        }
+    }
+    interface IWorkable
+    {
+        string Work();
+        bool IsWorking { get; }
+    }
+    interface IManager
+    {
+        List<Employee> ListOfWorker { get; set; }
+        void Organize();
+        void MakeBudgect();
+        void Control();
+    }
+    class Director : Employee, IManager//implement/ realize
+    {
+        public List<Employee> ListOfWorker { get; set; }
+        public void Control()
+        {
+            Console.WriteLine("Controling work!!!!");
+        }
+        public void MakeBudgect()
+        {
+            Console.WriteLine("Making money....");
+        }
+        public void Organize()
+        {
+            Console.WriteLine("I organize work....");
+        }
+    }
+    class Seller : Employee, IWorkable
+    {
+        public bool IsWorking => true;
+        public string Work()
+        {
+            return "I selling product!";
+        }
+    }
+    class Cashier : Employee, IWorkable
+    {
+        public bool IsWorking => true;
+        public string Work()
+        {
+            return "I get pay for product";
+        }
+    }
+    class Administrator : Employee, IWorkable, IManager
+    {
+        public bool IsWorking => true;
+        public List<Employee> ListOfWorker { get; set; }
+        public void Control()
+        {
+            Console.WriteLine("I am a BOSS. You must comply me!!!");
+        }
+        public void MakeBudgect()
+        {
+            Console.WriteLine("I have many money!!!!");
+        }
+        public void Organize()
+        {
+            Console.WriteLine("I organize work today");
+        }
+        public string Work()
+        {
+            return "I do my work(((((((";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Director director = new Director
+            //Interfaces reference
+            IManager director = new Director
+            {
+                FirstName = "Jeff",
+                LastName = "Bezos",
+                Birthdate = new DateTime(1964, 12, 4),
+                Position = "Ceo",
+                Salary = 5000000
+            };
+            // director.Salary = 1000000000;
+            Console.WriteLine(director);
+            director.Organize();
+            director.Control();
+            director.MakeBudgect();
+            director.ListOfWorker = new List<Employee>()
+            {
+                new Seller
+                {
+                    FirstName = "Tanya",
+                    LastName = "Oliunuk",
+                    Birthdate = new DateTime(2000, 12, 4),
+                    Position = "Seller",
+                    Salary = 8000
+                },
+                new Cashier
+                {
+                    FirstName = "Olga",
+                    LastName = "Oliunuk",
+                    Birthdate = new DateTime(2000, 12, 4),
+                    Position = "Seller",
+                    Salary = 9000
+                }
+            };
+
+            (director as Director)!.Salary = 30000000;
+            Console.WriteLine(director);
+
+            //Seller seller = new Seller
+            IWorkable seller = new Seller
+            {
+                FirstName = "Tanya",
+                LastName = "Oliunuk",
+                Birthdate = new DateTime(2000, 12, 4),
+                Position = "Seller",
+                Salary = 8000
+            };
+            //seller.Salary = 10000000000;
+            Console.WriteLine(seller);
+            seller.Work();
+
+            Administrator admin = new Administrator();
+
+            IManager manager = admin;
+            manager.Organize();
+
+            IWorkable worka = admin;
+            worka.Work();
+            //Example interfaces
+            //https://github.com/IliushynOlena/cs_interfaces
+        }
+    }
+}
